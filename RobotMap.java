60006b5c9748fb0af686a7b873ec06e971c300ad
Nick <Nick@Nicks-PC.jetpack>
2017-02-22 18:36:38 -0500

    2/22/17
    robot map and drivebase

---
+++ RobotMap.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team303.robot;
+
+public class RobotMap {
+
+	public static final int FR = 7;
+	public static final int FL = 8;
+	public static final int BR = 3;
+	public static final int BL = 4;
+	public static final boolean FR_INV = false;
+	public static final boolean FL_INV = true;
+	public static final boolean BR_INV = true;
+	public static final boolean BL_INV = false;
+	public static final int CLAW = 9;
+	public static final boolean CLAW_INV = false;
+	public static final int INTAKE = 10;
+	public static final boolean INTAKE_INV = true;
+	public static final int LCLAWWHEEL = 2;
+	public static final int RCLAWWHEEL = 5;
+	public static final int INTAKEWHEELS = 6;
+	public static final boolean INTAKEWHEELS_INV = false;
+	
+}
