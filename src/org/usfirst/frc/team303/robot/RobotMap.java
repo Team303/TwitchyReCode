02829d166acb0cd7c1b7ac24b15251f50be6962e
Nick <Nick@Nicks-PC.jetpack>
2017-02-22 19:03:11 -0500

    re iding the talons
    2/22/17

--- src/org/usfirst/frc/team303/robot/RobotMap.java
+++ src/org/usfirst/frc/team303/robot/RobotMap.java
@@ -2,21 +2,21 @@ package org.usfirst.frc.team303.robot;
 
 public class RobotMap {
 
-	public static final int FR = 7;
-	public static final int FL = 8;
-	public static final int BR = 3;
-	public static final int BL = 4;
+	public static final int FR = 2;
+	public static final int FL = 13;
+	public static final int BR = 55;
+	public static final int BL = 12;
 	public static final boolean FR_INV = false;
 	public static final boolean FL_INV = true;
 	public static final boolean BR_INV = true;
 	public static final boolean BL_INV = false;
-	public static final int CLAW = 9;
+	public static final int CLAW = 15;
 	public static final boolean CLAW_INV = false;
-	public static final int INTAKE = 10;
+	public static final int INTAKE = 3;
 	public static final boolean INTAKE_INV = true;
-	public static final int LCLAWWHEEL = 2;
-	public static final int RCLAWWHEEL = 5;
-	public static final int INTAKEWHEELS = 6;
+	public static final int LCLAWWHEEL = 14;
+	public static final int RCLAWWHEEL = 22;
+	public static final int INTAKEWHEELS = 4;
 	public static final boolean INTAKEWHEELS_INV = false;
 	
 }
